bb1cc7bb2ed37dc242f1455d19b655358649e386
Kurt Lopez <kurtdevph@gmail.com>
2019-04-13 13:08:19 +0800

    nutrition items

--- src/products/ProductListItem.js
+++ src/products/ProductListItem.js
@@ -7,6 +7,24 @@ class ProductListItem extends Component {
       <div className="product-card">
         <div key={this.props.id} className="product-card">
           <div className='product-image' style={{backgroundImage: `url(//${this.props.img})`}}>
+            <div className="nutrition-container">
+              <div className="nutrition">
+                <p>{this.props.calories}</p>
+                <div className="nutrition-label">Calories</div>
+              </div>
+              <div className="nutrition">
+                <p>{this.props.protein}</p>
+                <div className="nutrition-label">Protein</div>
+              </div>
+              <div className="nutrition">
+                <p>{this.props.carbs}</p>
+                <div className="nutrition-label">Carbs</div>
+              </div>
+              <div className="nutrition">
+                <p>{this.props.fat}</p>
+                <div className="nutrition-label">Fat</div>
+              </div>
+            </div>
           </div>
           <div className="product-info">
             <h4>{this.props.name}</h4>
