6077f1781a190e56e6768c5e5dab4633a015ed6c
Kurt Lopez <kurtdevph@gmail.com>
2019-04-13 18:38:21 +0800

    Updates

---
+++ precache-manifest.edf58d6ecc1cc91b1bf57ef5cbd69419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35f39512933c3d999efe2b5b8023587c",
+    "url": "/goodfoodph/static/media/arrow.35f39512.svg"
+  },
+  {
+    "revision": "49b9aeac88fbb14f7789",
+    "url": "/goodfoodph/static/js/runtime~main.6c466545.js"
+  },
+  {
+    "revision": "a6ca73645dec9a1914df",
+    "url": "/goodfoodph/static/js/main.07047489.chunk.js"
+  },
+  {
+    "revision": "af0b640cc2fb6793e3e3",
+    "url": "/goodfoodph/static/js/2.57c36ffa.chunk.js"
+  },
+  {
+    "revision": "a6ca73645dec9a1914df",
+    "url": "/goodfoodph/static/css/main.bf79e77a.chunk.css"
+  },
+  {
+    "revision": "c3d5d7e7b8296968212b8feb3dccee57",
+    "url": "/goodfoodph/index.html"
+  }
+];
