1d99c54189f23a55366ca86d339212b2735db18c
Kurt Lopez <kurtdevph@gmail.com>
2019-04-16 18:22:08 +0800

    menu url fix

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -25,12 +25,12 @@ export class Navbar extends Component {
       <nav>
         <div className="nav-container">
           <div className="logo">
-            <NavLink to="/">GoodFoodPh</NavLink>
+            <NavLink to={process.env.PUBLIC_URL + '/'}>GoodFoodPh</NavLink>
             <input onChange={this.filterProducts} type="text" placeholder="Search Products"></input>
           </div>
           <div className="menu-container">
             <ul>
-              <li><NavLink to="/">OUR Menu</NavLink></li>
+              <li><NavLink to={process.env.PUBLIC_URL + '/'}>OUR Menu</NavLink></li>
               <li><NavLink to="/contact">Contact</NavLink></li>
               <li><a onClick={(e)=> this.handleClick(e)} className={this.props.cartItems.length > 0 ? 'cart cart-active':'cart'}>Cart{totalQuantity > 0 ? <span>{totalQuantity}</span> : ''}</a></li>
             </ul>
