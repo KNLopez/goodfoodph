e8d4c5ac161ce7313011553049df4066295b9709
Kurt Lopez <kurtdevph@gmail.com>
2019-04-20 20:07:56 +0800

    actionTypes

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const REMOVE_TAG = 'REMOVE_TAG'
+export const ADD_TAG = 'ADD_TAG'
+export const SEARCH_FILTER_PRODUCTS = "SEARCH_FILTER_PRODUCTS"
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const TOGGLE_CART = 'TOGGLE_CART'
+export const SUBMIT_FORM = 'SUBMIT_FORM'
+export const ADJUST_QUANTITY = 'ADJUST_QUANTITY'
